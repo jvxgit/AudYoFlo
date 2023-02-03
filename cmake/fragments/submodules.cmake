8c987cc030d9d08c35c0646b488cde6a8f38c023
Hauke Krüger <hk@javox-solutions.com>
2023-02-03 17:41:41 +0100

    Added an "ignore list" to deactivate certain sub projects. CMake will first accept the "mysubmodules" list and then apply the "ignsubmodules" entries

--- cmake/fragments/submodules.cmake
+++ cmake/fragments/submodules.cmake
@@ -1,17 +1,24 @@
-	message("<< Checking for added submodules in folder <${JVXRT_SUBMODULE_PATH}> >>")
-	
-  	set(JVX_CONFIGURED_SUBMODULES "")
-
+	message("<< Checking for submodule settings in folder <${JVXRT_SUBMODULE_PATH}> >>")
+	if(mysubmodules)
+		message("<< Selected submodules: <${mysubmodules}> >>")
+	endif()
+	if(ignsubmodules)
+		message("<< Ignored submodules: <${ignsubmodules}> >>")
+	endif()
+  	
+	set(JVX_CONFIGURED_SUBMODULES_PRE "")
 	SUBDIRLIST(SUBDIRS ${JVXRT_SUBMODULE_PATH})
+
 	message ("--> Sub directories to be evaluated: ${SUBDIRS}")
+	
 	# LABEL ADD SUB_PROJECTS HERE!
 	FOREACH(subdir ${SUBDIRS})
 		if(mysubmodules)
-			message("Look for ${subdir} in list of allowed submodules: \"${mysubmodules}\"")
+			message("++> Look for ${subdir} in list of allowed submodules: \"${mysubmodules}\"")
 			list(FIND mysubmodules ${subdir} ifoundit)
 			if(${ifoundit} GREATER -1)
 				if(EXISTS ${JVXRT_SUBMODULE_PATH}/${subdir}/.jvxprj.base OR EXISTS ${JVXRT_SUBMODULE_PATH}/${subdir}/.jvxprj.audio OR EXISTS ${JVXRT_SUBMODULE_PATH}/${subdir}/.jvxprj)
-					set(JVX_CONFIGURED_SUBMODULES "${JVX_CONFIGURED_SUBMODULES};${subdir}")
+					set(JVX_CONFIGURED_SUBMODULES_PRE "${JVX_CONFIGURED_SUBMODULES_PRE};${subdir}")
 				else()
 					message("|--> .jvxprj.base|.jvxprj.audio|.jvxprj not found in ${JVXRT_SUBMODULE_PATH}/${subdir}, NOT Building folder.")
 				endif()
@@ -20,16 +27,33 @@
 			endif()
 		else()
 			if(EXISTS ${JVXRT_SUBMODULE_PATH}/${subdir}/.jvxprj.base OR EXISTS ${JVXRT_SUBMODULE_PATH}/${subdir}/.jvxprj.audio OR EXISTS ${JVXRT_SUBMODULE_PATH}/${subdir}/.jvxprj)
-				set(JVX_CONFIGURED_SUBMODULES "${JVX_CONFIGURED_SUBMODULES};${subdir}")
+				set(JVX_CONFIGURED_SUBMODULES_PRE "${JVX_CONFIGURED_SUBMODULES_PRE};${subdir}")
 			else()
 				message("|--> .jvxprj.base|.jvxprj.audio|.jvxprj not found in ${JVXRT_SUBMODULE_PATH}/${subdir}, NOT Building folder.")
 			endif()
 		endif()
 	ENDFOREACH()
 	
+	message("##> Step 1: Pre-selected submodules: ${JVX_CONFIGURED_SUBMODULES_PRE}")
+	if(ignsubmodules)	
+		FOREACH(entry ${JVX_CONFIGURED_SUBMODULES_PRE})
+		
+			# message("Look for ${entry} in list of ignored submodules: \"${ignsubmodules}\"")
+			list(FIND ignsubmodules ${entry} ifoundit)
+			if(${ifoundit} GREATER -1)
+				# Nothing, ignoring
+				message("|--> Project ${JVXRT_SUBMODULE_PATH}/${entry} is in list for ignored submodules - not building it.")
+			else()				
+				set(JVX_CONFIGURED_SUBMODULES "${JVX_CONFIGURED_SUBMODULES};${entry}")
+			endif()
+		ENDFOREACH()
+	else()
+		set(JVX_CONFIGURED_SUBMODULES "${JVX_CONFIGURED_SUBMODULES_PRE}")
+	endif()		
+
+	# The second variable may still be in use somewhere!
 	set(JVX_CONFIGURED_AUDIO_SUBMODULES ${JVX_CONFIGURED_SUBMODULES})
-	
-	## message(FATAL_ERROR "Valid submodules : ${JVX_CONFIGURED_AUDIO_SUBMODULES}")	
+
 	FOREACH(subdir ${JVX_CONFIGURED_SUBMODULES})
 		message("XX> Building sub project in folder ${JVXRT_SUBMODULE_PATH}/${subdir}")
 		set(JVX_FOLDER_HIERARCHIE_BASE_OLD ${JVX_FOLDER_HIERARCHIE_BASE})
@@ -37,3 +61,4 @@
 		add_subdirectory(${JVXRT_SUBMODULE_PATH}/${subdir} ${JVXRT_SUBPROJECTS_PATH_MAP}/${subdir})
 		set(JVX_FOLDER_HIERARCHIE_BASE ${JVX_FOLDER_HIERARCHIE_BASE_OLD})
 	ENDFOREACH()
+	
