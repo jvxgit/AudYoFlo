471139af5032f0bfd667bafb4277459459241260
Hauke Krüger <hk@javox-solutions.com>
2023-02-08 14:50:29 +0100

    Updated to VST SDK 3.7.7

--- patches/vst/prepareModules.sh
+++ patches/vst/prepareModules.sh
@@ -1,9 +1,10 @@
 #!/bin/bash  
 
 if [ ! -d "vst3sdk" ]; then
-	git clone --recursive --branch v3.7.2_build_28  https://github.com/steinbergmedia/vst3sdk.git
-	git apply -p0 ../../patches/vst/adapt_vst_repo-CMakeLists.patch
-	git apply -p0 ../../patches/vst/adapt_vst_repo-PluginInterfaces.patch
+	git clone --recursive --branch v3.7.7_build_19 https://github.com/steinbergmedia/vst3sdk.git
+	
+	patch -R -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-cml.patch
+	patch -R -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-cma.patch
 fi
 
 
