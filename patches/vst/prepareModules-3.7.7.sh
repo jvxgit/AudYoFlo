11c737aa5767fd6bed7ef77c78304f04582bcd3e
Hauke Krüger <hk@javox-solutions.com>
2023-02-10 15:40:52 +0100

    Added another patch to get the moduleinfo target dir from VST build process. We can use this later if our VST plugin requires a dll dependency

--- patches/vst/prepareModules-3.7.7.sh
+++ patches/vst/prepareModules-3.7.7.sh
@@ -5,6 +5,7 @@ if [ ! -d "vst3sdk" ]; then
 	git clone --recursive --branch v3.7.7_build_19 https://github.com/steinbergmedia/vst3sdk.git
 	patch -R -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-cml.patch
 	patch -R -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-cma.patch
+	patch -R -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-modutil-cml.patch
 fi
 
 
