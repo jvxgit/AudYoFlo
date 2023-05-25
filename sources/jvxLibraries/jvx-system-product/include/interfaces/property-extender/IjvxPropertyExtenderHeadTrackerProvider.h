b7a247b957b20c5cea0466c12373609121104430
Hauke Krüger <hk@javox-solutions.com>
2023-05-25 16:27:31 +0200

    Ups, forgot this file

---
+++ sources/jvxLibraries/jvx-system-product/include/interfaces/property-extender/IjvxPropertyExtenderHeadTrackerProvider.h
@@ -0,0 +1,20 @@
+#ifndef _IJVXPROPERTYEXTENDERHEADTRACKERPROVIDER_H_
+#define _IJVXPROPERTYEXTENDERHEADTRACKERPROVIDER_H_
+
+JVX_INTERFACE IjvxPropertyExtenderHeadTrackerProvider_report
+{
+public:
+	virtual ~IjvxPropertyExtenderHeadTrackerProvider_report() {};
+	virtual jvxErrorType report_rotation_angle(jvxData newValDegree) = 0;
+};
+
+JVX_INTERFACE IjvxPropertyExtenderHeadTrackerProvider
+{
+public:
+	virtual ~IjvxPropertyExtenderHeadTrackerProvider() {};
+	virtual jvxErrorType register_listener(IjvxPropertyExtenderHeadTrackerProvider_report* ptr, const char* tag) = 0;
+	virtual jvxErrorType unregister_listener(IjvxPropertyExtenderHeadTrackerProvider_report* ptr) = 0;
+};
+
+#endif
+		
