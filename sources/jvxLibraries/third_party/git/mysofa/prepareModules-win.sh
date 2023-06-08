db6d71db79a84c139bc666a9f9b4c22b63cd9c79
Hauke Krüger <hk@javox-solutions.com>
2023-06-08 17:12:55 +0200

    Made the commit id of sofa lib fixed

--- sources/jvxLibraries/third_party/git/mysofa/prepareModules-win.sh
+++ sources/jvxLibraries/third_party/git/mysofa/prepareModules-win.sh
@@ -3,6 +3,10 @@
 if [ ! -d "libmysofa" ]; then
 	git clone https://github.com/hoene/libmysofa.git
 
+	cd libmysofa
+	git checkout  bed445ba48ad9faf0e758b54af3f6075949d27fe
+	cd ..
+	
 	# Really hard to patch this - patch tools are really annoying. Something with linefeeds
 	patch -R -p0  --binary < patch-src.patch.win
 	patch -R -p0  --binary < patch-cml.patch.win
