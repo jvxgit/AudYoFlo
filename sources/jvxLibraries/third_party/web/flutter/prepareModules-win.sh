bde36a7bae0ec338e15150c37ed037515677489d
Hauke Krüger <hk@javox-solutions.com>
2023-07-07 13:37:28 +0200

    Removed error report if running build without latex /dvips

--- sources/jvxLibraries/third_party/web/flutter/prepareModules-win.sh
+++ sources/jvxLibraries/third_party/web/flutter/prepareModules-win.sh
@@ -1,6 +1,7 @@
 #!/bin/bash  
 
 if [ ! -d "flutter-3.7.4" ]; then
+
 	curl -kLSs  https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.7.4-stable.zip -o flutter.zip	
 	unzip flutter.zip
 	mv flutter flutter-3.7.4
