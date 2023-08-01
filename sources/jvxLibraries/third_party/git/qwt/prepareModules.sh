00c44391b26c8583928369cdefb2fe1658923dce
Hauke Krüger <hk@javox-solutions.com>
2023-08-01 10:04:47 +0200

    Removed old patch - qwt seems to work properly

--- sources/jvxLibraries/third_party/git/qwt/prepareModules.sh
+++ sources/jvxLibraries/third_party/git/qwt/prepareModules.sh
@@ -3,6 +3,6 @@
 if [ ! -d "qwt" ]; then
 	git clone -b develop https://git.code.sf.net/p/qwt/git qwt
 	git checkout 9e554a5e03bc3a1fe379bdddfa4d21ced2ed7995
-	# remove below line and patch file if new qwt version works out of the box
-	## patch -p0 < patch-compile-windows-16012023.patch
+	# with the latest version, no patch is required!
+	## patch -p0 < patch-src.patch
 fi
