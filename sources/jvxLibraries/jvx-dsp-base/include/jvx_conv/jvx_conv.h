6333b0f00f3f6ee77a6a2e7f9e9725bf8f2d7124
Hauke Krüger <hk@javox-solutions.com>
2023-08-17 17:45:22 +0200

    Added a second convulation function with two inputs / output added

--- sources/jvxLibraries/jvx-dsp-base/include/jvx_conv/jvx_conv.h
+++ sources/jvxLibraries/jvx-dsp-base/include/jvx_conv/jvx_conv.h
@@ -8,8 +8,14 @@ JVX_DSP_LIB_BEGIN
 jvxData 
 jvx_compute_conv_energy(jvxData* in1, jvxSize nin1, jvxData* in2, jvxSize nin2);
 
+jvxData
+jvx_compute_conv_energy_add(jvxData* inF1, jvxData* inF2, jvxSize ninF, jvxData* inS1, jvxData* inS2, jvxSize ninS);
+
 void
 jvx_compute_conv(jvxData* in1, jvxSize nin1, jvxData* in2, jvxSize nin2, jvxData* out, jvxSize nout);
+
+void
+jvx_compute_conv_add(jvxData* inF1, jvxData* inF2, jvxSize ninF, jvxData* inS1, jvxData* inS2, jvxSize ninS, jvxData* out, jvxSize nout);
 
 JVX_DSP_LIB_END
 
--- sources/jvxLibraries/jvx-dsp-base/src/jvx_conv/jvx_conv.c
+++ sources/jvxLibraries/jvx-dsp-base/src/jvx_conv/jvx_conv.c
@@ -62,6 +62,79 @@ jvx_compute_conv_energy(jvxData* inF, jvxSize ninF, jvxData* inS, jvxSize ninS)
 	return energy;
 }
 
+// =============================================================================================
+
+jvxData
+jvx_compute_conv_energy_add(jvxData* inF1, jvxData* inF2, jvxSize ninF, jvxData* inS1, jvxData* inS2, jvxSize ninS)
+{
+	jvxData energy = 0;
+	jvxData accu;
+	int i0, i, j; // must be int since it might be negative check
+
+	// Implementation expects that the first arrays size is greater or equal to second array size.
+	// If program is called the other way around, we swap arguments quickly to coply since convoultion 
+	// is commutative.
+	jvxData* in11 = inF1;
+	jvxData* in12 = inF2;
+	jvxSize nin1 = ninF;
+	jvxData* in21 = inS1;
+	jvxData* in22 = inS2;
+	jvxSize nin2 = ninS;
+	if (nin2 > nin1)
+	{
+		in11 = inS1;
+		in12 = inS2;
+		nin1 = ninS;
+		in21 = inF1;
+		in22 = inF2;
+		nin2 = ninF;
+	}
+
+	for (i0 = 0; i0 < (nin2 - 1); i0++)
+	{
+		accu = 0;
+		jvxData* ptr11 = in11;
+		jvxData* ptr21 = in21 + i0;
+		jvxData* ptr12 = in12;
+		jvxData* ptr22 = in22 + i0;
+		for (j = 0; j <= i0; j++)
+		{
+			accu += *ptr11++ * *ptr21-- + *ptr12++ * *ptr22--;
+		}
+		energy += accu * accu;
+	}
+
+	// This part may also be skipped if second vector is larger than first part
+	for (i = 0; i <= ((int)nin1 - (int)nin2); i++)
+	{
+		accu = 0;
+		jvxData* ptr11 = in11 + i;
+		jvxData* ptr21 = in21 + (nin2 - 1); // Point to last element
+		jvxData* ptr12 = in12 + i;
+		jvxData* ptr22 = in22 + (nin2 - 1); // Point to last element
+		for (j = 0; j < nin2; j++)
+		{
+			accu += *ptr11++ * *ptr21-- + *ptr12++ * *ptr22--;
+		}
+		energy += accu * accu;
+	}
+
+	for (i = 0; i < (nin2 - 1); i++, i0--)
+	{
+		accu = 0;
+		jvxData* ptr11 = in11 + nin1 - i0; // Towards end of buffer
+		jvxData* ptr21 = in21 + (nin2 - 1); // Point to last element
+		jvxData* ptr12 = in12 + nin1 - i0; // Towards end of buffer
+		jvxData* ptr22 = in22 + (nin2 - 1); // Point to last element
+		for (j = 0; j < i0; j++)
+		{
+			accu += *ptr11++ * *ptr21-- + *ptr12++ * *ptr22--;
+		}
+		energy += accu * accu;
+	}
+	return energy;
+}
+
 void
 jvx_compute_conv(jvxData* inF, jvxSize ninF, jvxData* inS, jvxSize ninS, jvxData* out, jvxSize nout)
 {
@@ -121,6 +194,80 @@ jvx_compute_conv(jvxData* inF, jvxSize ninF, jvxData* inS, jvxSize ninS, jvxData
 		for (j = 0; j < i0; j++)
 		{
 			accu += *ptr1++ * *ptr2--;
+		}
+		*out++ = accu;
+	}
+}
+
+void
+jvx_compute_conv_add(jvxData* inF1, jvxData* inF2, jvxSize ninF, jvxData* inS1, jvxData* inS2, jvxSize ninS, jvxData* out, jvxSize nout)
+{
+	// nout != nin2 - 1 + nin1 - nin2 + 1 + nin2 - 1
+	//       = nin2 - 1 + nin1 
+
+	jvxData accu;
+	int i0, i, j; // must be int since it might be negative check
+
+	// Implementation expects that the first arrays size is greater or equal to second array size.
+// If program is called the other way around, we swap arguments quickly to coply since convoultion 
+// is commutative.
+	jvxData* in11 = inF1;
+	jvxData* in12 = inF2;
+	jvxSize nin1 = ninF;
+	jvxData* in21 = inS1;
+	jvxData* in22 = inS2;
+	jvxSize nin2 = ninS;
+	if (nin2 > nin1)
+	{
+		in11 = inS1;
+		in12 = inS2;
+		nin1 = ninS;
+		in21 = inF1;
+		in22 = inF2;
+		nin2 = ninF;
+	}
+
+	assert(nout == nin1 + nin2 - 1);
+
+	for (i0 = 0; i0 < (nin2 - 1); i0++)
+	{
+		accu = 0;
+		jvxData* ptr11 = in11;
+		jvxData* ptr21 = in21 + i0;
+		jvxData* ptr12 = in12;
+		jvxData* ptr22 = in22 + i0;
+		for (j = 0; j <= i0; j++)
+		{
+			accu += *ptr11++ * *ptr21-- + *ptr12++ * *ptr22--;
+		}
+		*out++ = accu;
+	}
+
+	// This part may also be skipped if second vector is larger than first part
+	for (i = 0; i <= ((int)nin1 - (int)nin2); i++)
+	{
+		accu = 0;
+		jvxData* ptr11 = in11 + i;
+		jvxData* ptr21 = in21 + (nin2 - 1); // Point to last element
+		jvxData* ptr12 = in12 + i;
+		jvxData* ptr22 = in22 + (nin2 - 1); // Point to last element
+		for (j = 0; j < nin2; j++)
+		{
+			accu += *ptr11++ * *ptr21-- + *ptr12++ * *ptr22--;
+		}
+		*out++ = accu;
+	}
+
+	for (i = 0; i < (nin2 - 1); i++, i0--)
+	{
+		accu = 0;
+		jvxData* ptr11 = in11 + nin1 - i0; // Towards end of buffer
+		jvxData* ptr21 = in21 + (nin2 - 1); // Point to last element
+		jvxData* ptr12 = in12 + nin1 - i0; // Towards end of buffer
+		jvxData* ptr22 = in22 + (nin2 - 1); // Point to last element
+		for (j = 0; j < i0; j++)
+		{
+			accu += *ptr11++ * *ptr21-- + *ptr12++ * *ptr22--;
 		}
 		*out++ = accu;
 	}
