871d3a2f95e0563e5a5b30e97592cdcd5f3ea622
Hauke Krüger <hk@javox-solutions.com>
2023-08-23 08:42:01 +0200

    flutter checkout failed some times - seemed to be a race condition. I added a sleep to make sure it no longer fails

--- sources/jvxLibraries/third_party/web/flutter/prepareModules-win.sh
+++ sources/jvxLibraries/third_party/web/flutter/prepareModules-win.sh
@@ -4,6 +4,7 @@ if [ ! -d "flutter-3.7.4" ]; then
 
 	curl -kLSs  https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.7.4-stable.zip -o flutter.zip	
 	unzip flutter.zip
+	sleep 1
 	mv flutter flutter-3.7.4
 	rm flutter.zip
 fi
