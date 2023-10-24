e42d73226d1939ea5f4e697abcec87fa54a45bd5
Hauke Krüger <hk@javox-solutions.com>
2023-10-24 18:37:19 +0200

    Another fix for hdf5 zlib problems

--- sources/jvxLibraries/third_party/web/hdf5/build/compile.bat
+++ sources/jvxLibraries/third_party/web/hdf5/build/compile.bat
@@ -2,11 +2,12 @@ echo off
 REM set arch=x64
 set arch=%~1
 
-set zlib_root="%CD%\..\..\..\platform-packages\msvc\zlib\msvc2017_%arch%\lib\zlib\zlib.lib"
+set zlib_root_lib="%CD%\..\..\..\platform-packages\msvc\zlib\msvc2017_%arch%\lib\zlib\zlib.lib"
+set zlib_root_incdir="%CD%\..\..\..\platform-packages\msvc\zlib\msvc2017_%arch%\include\zlib"
 REM set generatortoken=-G "Visual Studio 16 2019" -A "win32"
 set generatortoken=-G "Ninja"
 
 del CMakeCache.txt
-echo cmake %generatortoken% -DZLIB_LIBRARY=%zlib_root% -DCMAKE_INSTALL_PREFIX="../%~2/%arch%" -DCMAKE_BUILD_TYPE=Debug ../%~2
-cmake %generatortoken% -DZLIB_LIBRARY=%zlib_root% -DCMAKE_INSTALL_PREFIX="../%~2/%arch%" -DCMAKE_BUILD_TYPE=Debug ../%~2
+echo cmake %generatortoken% -DZLIB_LIBRARY=%zlib_root_lib% -DZLIB_INCLUDE_DIR=%zlib_root_incdir% -DCMAKE_INSTALL_PREFIX="../%~2/%arch%" -DCMAKE_BUILD_TYPE=Debug ../%~2
+cmake %generatortoken% -DZLIB_LIBRARY=%zlib_root_lib% -DZLIB_INCLUDE_DIR=%zlib_root_incdir% -DCMAKE_INSTALL_PREFIX="../%~2/%arch%" -DCMAKE_BUILD_TYPE=Debug ../%~2
 ninja install
