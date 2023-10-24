a622300c0574f3520edd6f66c1c01a4b7fbcda59
Hauke Krüger <hk@binauricsaudio.com>
2023-10-24 18:23:43 +0200

    1) Added common zlib location - which is not yet used by sofa project. Need to fix that 2) Last modification to hdf5 to build in a stable way for Windows win32 and x64

--- sources/jvxLibraries/third_party/web/hdf5/build/compile.bat
+++ sources/jvxLibraries/third_party/web/hdf5/build/compile.bat
@@ -1,15 +1,12 @@
 echo off
 REM set arch=x64
 set arch=%~1
-REM set rel=MDd
-set rel=%~2
 
-REM set zlib_root="%CD%\..\zlib-win64\lib\zlibstat.lib"
-set zlib_root="%CD%\..\zlib-windows\binaries\%arch%\zlibstat%rel%.lib"
+set zlib_root="%CD%\..\..\..\platform-packages\msvc\zlib\msvc2017_%arch%\lib\zlib\zlib.lib"
+REM set generatortoken=-G "Visual Studio 16 2019" -A "win32"
+set generatortoken=-G "Ninja"
 
-REM echo cmake -G "Visual Studio 17 2022" -A "x64" -DZLIB_LIBRARY=%zlib_root% -DCMAKE_INSTALL_PREFIX=".\x64" -DCMAKE_BUILD_TYPE=Debug ..\hdf5-1.14.2
-REM cmake -G "Visual Studio 17 2022" -A "x64" -DZLIB_LIBRARY=%zlib_root% -DCMAKE_INSTALL_PREFIX=".\x64" -DCMAKE_BUILD_TYPE=Debug ..\hdf5-1.14.2
-
-echo cmake -G "Ninja" -DZLIB_LIBRARY=%zlib_root% -DCMAKE_INSTALL_PREFIX="../%~3/%arch%_%rel%" -DCMAKE_BUILD_TYPE=Debug ../%~3
-cmake -G "Ninja" -DZLIB_LIBRARY=%zlib_root% -DCMAKE_INSTALL_PREFIX="../%~3/%arch%_%rel%" -DCMAKE_BUILD_TYPE=Debug ../%~3
+del CMakeCache.txt
+echo cmake %generatortoken% -DZLIB_LIBRARY=%zlib_root% -DCMAKE_INSTALL_PREFIX="../%~2/%arch%" -DCMAKE_BUILD_TYPE=Debug ../%~2
+cmake %generatortoken% -DZLIB_LIBRARY=%zlib_root% -DCMAKE_INSTALL_PREFIX="../%~2/%arch%" -DCMAKE_BUILD_TYPE=Debug ../%~2
 ninja install
--- sources/jvxLibraries/third_party/web/hdf5/prepareModules-win.bat
+++ sources/jvxLibraries/third_party/web/hdf5/prepareModules-win.bat
@@ -1,2 +1,7 @@
-cd build
-compile.bat "%~1" "%~2" "%~3"
+if not exist "build%~1" mkdir "build%~1"
+
+copy build\compile.bat "build%~1"
+cd "build%~1"
+echo compile.bat "%~1" "%~2"
+compile.bat "%~1" "%~2"
+
