e8ad299b020f8c600e9db459be43f281b1ca2d0b
Hauke Krüger <hk@javox-solutions.com>
2023-11-19 15:55:48 +0100

    Added another patch to build in Linux

--- patches/vst/prepareModules-3.7.7.sh
+++ patches/vst/prepareModules-3.7.7.sh
@@ -3,9 +3,18 @@
 if [ ! -d "vst3sdk" ]; then
 	echo git clone --recursive --branch v3.7.7_build_19 https://github.com/steinbergmedia/vst3sdk.git
 	git clone --recursive --branch v3.7.7_build_19 https://github.com/steinbergmedia/vst3sdk.git
+	
+	echo patch -R -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-cml.patch
 	patch -R -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-cml.patch
+	
+	echo patch -R -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-cma.patch
 	patch -R -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-cma.patch
+	
+	echo patch -R -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-modutil-cml.patch
 	patch -R -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-modutil-cml.patch
+	
+	echo patch -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-modinfo.patch
+	patch -p0 --binary < ../../patches/vst/patch.vstsdk-v3.7.7-modinfo.patch
 fi
 
 
