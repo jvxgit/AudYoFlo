856777ce7ad436465034d9daca08a924517e97c6
Hauke Krüger <hk@binauricsaudio.com>
2024-04-15 19:19:53 +0200

    Improvement of windows build for orc - to support 32 and 64 bit builds from within one folder

--- sources/jvxLibraries/third_party/git/orc/prepareModules-win.bat
+++ sources/jvxLibraries/third_party/git/orc/prepareModules-win.bat
@@ -1,11 +1,15 @@
 echo on
 
-echo Running ORC meson build for install folder %~1
+echo Running ORC meson build for install folder %~1 %~2
 cd orc
 
-REM python ..\meson\meson.py setup build -Ddefault_library=static --backend vs --prefix=%~1 
-python ..\meson\meson.py setup build -Ddefault_library=shared --prefix=%~1 
+python ..\meson\meson.py setup build%~2 -Ddefault_library=static --reconfigure --prefix=%~1 
 
-cd build
+cd build%~2
 python ..\..\meson\meson.py install
+
+cd ..
+
+
+
 	
