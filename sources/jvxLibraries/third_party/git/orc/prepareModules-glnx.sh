f2accee0fc79c5f232363f3e67966535ae63e328
Hauke Krüger <hk@binauricsaudio.com>
2024-04-15 20:54:01 +0200

    another round orc fixing

--- sources/jvxLibraries/third_party/git/orc/prepareModules-glnx.sh
+++ sources/jvxLibraries/third_party/git/orc/prepareModules-glnx.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ ! -d "orc" ]; then
+if [ ! -d "orc/$2" ]; then
 	git clone https://github.com/GStreamer/orc
 	pushd .
 	cd orc
 
-	meson setup build -Ddefault_library=static --prefix=$1
-	echo meson setup build -Ddefault_library=static --prefix=$1
+	meson setup build$2 -Ddefault_library=static --prefix=$1
+	echo meson setup build$2 -Ddefault_library=static --prefix=$1
 
-	cd build
+	cd build$2
 	
 	meson install
 	echo meson install
