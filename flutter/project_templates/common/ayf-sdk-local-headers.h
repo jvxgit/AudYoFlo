0aeff009de6924416a8c387435cf6cf98c543d43
Hauke Krüger <hk@binauricsaudio.com>
2024-06-01 17:41:48 +0200

    Extended SDK generation to flutter template files

--- flutter/project_templates/common/ayf-sdk-local-headers.h
+++ flutter/project_templates/common/ayf-sdk-local-headers.h
@@ -1,10 +1,13 @@
 #ifndef __AYF_SDK_LOCAL_HEADERS_H__
 #define __AYF_SDK_LOCAL_HEADERS_H__
 
+#define STRINGIZE_(a) #a
+#define STRINGIZE(a) STRINGIZE_(a)
+
 #define JVX_COMPONENT_ACCESS_CALLING_CONVENTION 
 #include "jvx_sdk.h"
 #include "jvx.h"
 #include "flutter_native_host_config.h"
-#include "ayfstarter-native-config.h"
+#include STRINGIZE(AYF_PROJECT.h)
 
 #endif
