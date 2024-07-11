28c8f3ce84f36d94ebcea8ac861bb35168c406e3
Hauke Krüger <hk@javox-solutions.com>
2024-07-11 15:48:23 +0200

    1) Orc with correct runtime libs to interact with AudYoFlo 2) ffmpeg to exted using libx264 and libx265 - HINT: this version now is onthe road for gpl. Hiwever, I intend to come back to lgpl later when replacing the libx264

--- sources/jvxLibraries/third_party/git/orc/prepareModules-win.bat
+++ sources/jvxLibraries/third_party/git/orc/prepareModules-win.bat
@@ -3,7 +3,7 @@ echo on
 echo Running ORC meson build for install folder %~1 %~2
 cd orc
 
-python ..\meson\meson.py setup build%~2 -Ddefault_library=static --reconfigure --prefix=%~1 
+python ..\meson\meson.py setup build%~2 -Ddefault_library=static -Db_vscrt=mdd --reconfigure --prefix=%~1 
 
 cd build%~2
 python ..\..\meson\meson.py install
--- sources/jvxPackages/ffmpeg/jvxLibraries/third_party/git/ffmpeg/prepareModules-win.sh
+++ sources/jvxPackages/ffmpeg/jvxLibraries/third_party/git/ffmpeg/prepareModules-win.sh
@@ -3,11 +3,9 @@
 # Rn this script as follows:
 # msys2_shell.cmd -use-full-path -c "cd ${CMAKE_CURRENT_SOURCE_DIR} ; ./prepareModules-win.sh"
 
-#postfix="x64"
-#if [ $# -ge 1 ]; then 
-	postfix=$1
-# fi
-
+postfix=$1
+release_mode=$2
+compile_flags=$3
 folder="ffmpeg-$postfix"
 
 # Activate the verbose mode!!
@@ -45,7 +43,45 @@ if [ -d $folder ]; then
 	echo "Checking for existence of subfolder lib"
 	if [ ! -d "lib" ]; then
 		
-		./configure --target-os=win64 --arch=x86_64 --toolchain=msvc --prefix=./ --disable-htmlpages --disable-manpages
+		if [ ! -d "x264/install-$release_mode" ]; then
+			# Extensions in ffmpeg to install libx264
+			# -> https://www.roxlu.com/2016/057/compiling-x264-on-windows-with-msvc		
+			git clone http://git.videolan.org/git/x264.git
+			pushd .
+			cd x264
+			CC=cl ./configure --enable-static --extra-cflags=$compile_flags --prefix=${PWD}/install-$release_mode
+			make 
+			make install
+			popd
+		fi
+		
+		export PKG_CONFIG_PATH=${PWD}/x264/install-$release_mode/lib/pkgconfig:$PKG_CONFIG_PATH
+		 
+		if [ ! -d "x265_git/build/msys-cl/install-$release_mode" ]; then
+			git clone https://bitbucket.org/multicoreware/x265_git.git
+			pushd .
+			cd x265_git/build/msys-cl
+		
+			# The following line can be modified to run directly
+			# ./make-Makefiles-64bit.sh
+			cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-DWIN32 -D_WINDOWS -W4 -GR -EHsc" -DCMAKE_C_FLAGS="-DWIN32 -D_WINDOWS -W4 $compile_flags"  ../../source -DCMAKE_INSTALL_PREFIX=./install-$release_mode
+			nmake 
+			nmake install
+		
+			cp install-$release_mode/lib/libx265.lib install-$release_mode/lib/x265.lib
+			popd
+		fi
+
+		export PKG_CONFIG_PATH=${PWD}/x265_git/build/msys-cl/install-$release_mode/lib/pkgconfig:$PKG_CONFIG_PATH
+		echo PKG_CONFIG_PATH = $PKG_CONFIG_PATH
+	    
+		## Test pkf-config of a problem is reported
+		## pkg-config --debug x264
+
+		# H264 -> 
+		# https://stackoverflow.com/questions/50693934/different-h264-encoders-in-ffmpeg
+		# https://superuser.com/questions/512368/unknown-encoder-libx264-on-windows
+		./configure --target-os=win64 --arch=x86_64 --toolchain=msvc --enable-gpl --enable-libx264 --enable-libx265 --extra-cflags=-MDd --prefix=./ --disable-htmlpages --disable-manpages
 		
 		#echo ./configure --target-os=win64 --arch=x86_64 --enable-debug=3 --toolchain=msvc --prefix=./
 		#./configure --target-os=win64 --arch=x86_64 --enable-debug=3 --toolchain=msvc --prefix=./
@@ -65,9 +101,12 @@ if [ -d $folder ]; then
 		# Patches:
 		#
 		#
-
+		# Warning: O:\ayfdevelop\AudYoFlo\sources\jvxPackages\ffmpeg\jvxLibraries\third_party\git\ffmpeg\ffmpeg-x64\config.h(1): warning C4828: Die Datei enthält ein Zeichen, das bei Offset 0x20e beginnt. Dieses Zeichen ist im aktuellen Quellzeichensatz (Codepage 65001) unzulässig
+		# We can remove this warning by removing the "für" in config.h, assignment CC_IDENT
+		#
+		
 		echo make
-		make
+		make -j8
 	
 		echo make install
 		make install
