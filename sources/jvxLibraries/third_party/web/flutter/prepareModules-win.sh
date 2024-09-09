eab10ae72d96cde22ee442b9ac96567b0fe54f4a
Hauke Krüger <hk@javox-solutions.com>
2024-09-09 11:46:16 +0200

    More robustness for flutter download and extract

--- sources/jvxLibraries/third_party/web/flutter/prepareModules-win.sh
+++ sources/jvxLibraries/third_party/web/flutter/prepareModules-win.sh
@@ -2,10 +2,25 @@
 
 if [ ! -d "flutter-3.7.4" ]; then
 
-	curl -kLSs  https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.7.4-stable.zip -o flutter.zip	
-	unzip flutter.zip
+	if [ ! -d "flutter" ]; then
+		curl -kLSs  https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.7.4-stable.zip -o flutter.zip	
+		unzip flutter.zip
+	fi 
+	
 	sleep 5
-	mv flutter flutter-3.7.4
+	
+	max=10
+	for i in {1..$max}
+	do
+		if [ ! -d "flutter-3.7.4" ]; then
+			echo "Trying to mv flutter to flutter-3.7.4, rum <$i>"
+			mv flutter flutter-3.7.4
+			if [ ! -d "flutter-3.7.4" ]; then
+				sleep 5
+			fi 
+		fi 
+	done
+		
 	rm flutter.zip
 fi
 
