f30fac5b8ce085b2948ffc420598a2b09b9ec88c
Hauke Krüger <hk@javox-solutions.com>
2024-09-10 07:14:25 +0200

    On the build of openCV, moved the cleanup into CMakeLists.txt

--- sources/jvxLibraries/third_party/git/opencv/prepareModules-win.bat
+++ sources/jvxLibraries/third_party/git/opencv/prepareModules-win.bat
@@ -1,3 +1,5 @@
+echo on
+
 echo running prepareModules-win.bat with arguments %~1, %~2 and %~3
 if not exist "%~2\build_%~1" mkdir "%~2\build_%~1"
 
@@ -6,10 +8,3 @@ cd "%~2\build_%~1"
 echo compile.bat "%~1" "%~2" "%~3"
 compile.bat "%~1" "%~2" "%~3"
 
-cd ..
-
-echo %CD%
-dir 
-
-echo rmdir /S /Q build_%~1
-rmdir /S /Q build_%~1
