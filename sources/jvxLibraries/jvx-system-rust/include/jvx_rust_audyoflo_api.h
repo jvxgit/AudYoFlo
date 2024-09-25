7368373aa14b2c4deabd653706a364b97cec5068
Hauke Krüger <hk@binauricsaudio.com>
2024-09-25 15:55:05 +0200

    Addd glue code for rust headers

---
+++ sources/jvxLibraries/jvx-system-rust/include/jvx_rust_audyoflo_api.h
@@ -0,0 +1,9 @@
+#ifndef __JVX_RUST_AUDYOFLO_C_API_H__
+#define __JVX_RUST_AUDYOFLO_C_API_H__
+
+typedef jvxData JvxData;
+typedef jvxSize JvxSize;
+typedef jvxDspBaseErrorType JvxDspBaseErrorType;
+typedef jvxCBool JvxCBool;
+
+#endif
