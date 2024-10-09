a0bc51ba96fcdb614a49617f0db0d3f70bc6e549
Florian Wickert <fw@ferncast.de>
2024-10-09 14:13:31 +0200

    cbmp: fix prepareModules script

--- sources/jvxLibraries/third_party/git/cbmp/prepareModules.sh
+++ sources/jvxLibraries/third_party/git/cbmp/prepareModules.sh
@@ -1,8 +1,8 @@
 #!/bin/bash  
 
-if [ ! -d "cbmt" ]; then
+if [ ! -d "cbmp" ]; then
 	git clone https://github.com/mattflow/cbmp.git
-	cd cbmt
+	cd cbmp
 	git apply ../0001-Modified-lib-to-not-exit-in-case-a-bmp-was-not-valid.patch
 	cd ..
 fi
