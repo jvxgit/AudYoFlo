25b694d03811f1e94a69115c297368fda61ddd94
Hauke Krüger <hk@binauricsaudio.com>
2024-11-06 18:08:52 +0100

    Added function for convolution with limited size

--- sources/jvxLibraries/jvx-dsp-base/include/jvx_conv/jvx_conv.h
+++ sources/jvxLibraries/jvx-dsp-base/include/jvx_conv/jvx_conv.h
@@ -18,6 +18,9 @@ void
 jvx_compute_conv(jvxData* in1, jvxSize nin1, jvxData* in2, jvxSize nin2, jvxData* out, jvxSize nout);
 
 void
+jvx_compute_conv_constrained_size(jvxData* inF, jvxSize ninF, jvxData* inS, jvxSize ninS, jvxData* out, jvxSize nout);
+
+void
 jvx_compute_conv_add(jvxData* inF1, jvxData* inF2, jvxSize ninF, jvxData* inS1, jvxData* inS2, jvxSize ninS, jvxData* out, jvxSize nout);
 
 JVX_DSP_LIB_END
--- sources/jvxLibraries/jvx-dsp-base/src/jvx_conv/jvx_conv.c
+++ sources/jvxLibraries/jvx-dsp-base/src/jvx_conv/jvx_conv.c
@@ -213,6 +213,79 @@ jvx_compute_conv(jvxData* inF, jvxSize ninF, jvxData* inS, jvxSize ninS, jvxData
 }
 
 void
+jvx_compute_conv_constrained_size(jvxData* inF, jvxSize ninF, jvxData* inS, jvxSize ninS, jvxData* out, jvxSize nout)
+{
+	// nout != nin2 - 1 + nin1 - nin2 + 1 + nin2 - 1
+	//       = nin2 - 1 + nin1 
+	jvxSize outCnt = 0;
+	jvxData accu;
+	int i0, i, j; // must be int since it might be negative check
+
+	// Implementation expects that the first arrays size is greater or equal to second array size.
+// If program is called the other way around, we swap arguments quickly to coply since convoultion 
+// is commutative.
+	jvxData* in1 = inF;
+	jvxSize nin1 = ninF;
+	jvxData* in2 = inS;
+	jvxSize nin2 = ninS;
+	if (nin2 > nin1)
+	{
+		in1 = inS;
+		nin1 = ninS;
+		in2 = inF;
+		nin2 = ninF;
+	}
+
+	for (i0 = 0; i0 < (nin2 - 1); i0++)
+	{
+		accu = 0;
+		jvxData* ptr1 = in1;
+		jvxData* ptr2 = in2 + i0;
+		for (j = 0; j <= i0; j++)
+		{
+			accu += *ptr1++ * *ptr2--;
+		}
+		if (outCnt < nout)
+		{
+			*out++ = accu;
+			outCnt++;
+		}
+	}
+
+	// This part may also be skipped if second vector is larger than first part
+	for (i = 0; i <= ((int)nin1 - (int)nin2); i++)
+	{
+		accu = 0;
+		jvxData* ptr1 = in1 + i;
+		jvxData* ptr2 = in2 + (nin2 - 1); // Point to last element
+		for (j = 0; j < nin2; j++)
+		{
+			accu += *ptr1++ * *ptr2--;
+		}
+		if (outCnt < nout)
+		{
+			*out++ = accu;
+			outCnt++;
+		}
+	}
+
+	for (i = 0; i < (nin2 - 1); i++, i0--)
+	{
+		accu = 0;
+		jvxData* ptr1 = in1 + nin1 - i0; // Towards end of buffer
+		jvxData* ptr2 = in2 + (nin2 - 1); // Point to last element
+		for (j = 0; j < i0; j++)
+		{
+			accu += *ptr1++ * *ptr2--;
+		}
+		if (outCnt < nout)
+		{
+			*out++ = accu;
+			outCnt++;
+		}
+	}
+}
+void
 jvx_compute_conv_add(jvxData* inF1, jvxData* inF2, jvxSize ninF, jvxData* inS1, jvxData* inS2, jvxSize ninS, jvxData* out, jvxSize nout)
 {
 	// nout != nin2 - 1 + nin1 - nin2 + 1 + nin2 - 1
