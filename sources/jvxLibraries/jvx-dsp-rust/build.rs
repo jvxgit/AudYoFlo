ec1158eeec6a2c09e0116b828b873f7b620f344f
Florian Wickert <fw@ferncast.de>
2024-11-11 17:07:47 +0100

    fixup! jvx-dsp-rust: add build.rs script and readme to be able to run tests

--- sources/jvxLibraries/jvx-dsp-rust/build.rs
+++ sources/jvxLibraries/jvx-dsp-rust/build.rs
@@ -1,10 +1,12 @@
 fn main() {
-    // Link the static library
-    println!("cargo:rustc-link-lib=static=jvx-dsp-base_static");
-    println!("cargo:rustc-link-lib=static=jvx-dsp_static");
-    println!("cargo:rustc-link-lib=dylib=fftw3");
-    println!("cargo:rustc-link-lib=dylib=fftw3f");
+    if std::env::var("CARGO_FEATURE_TESTING").is_ok() {
+        // Link the static library
+        println!("cargo:rustc-link-lib=static=jvx-dsp-base_static");
+        println!("cargo:rustc-link-lib=static=jvx-dsp_static");
+        println!("cargo:rustc-link-lib=dylib=fftw3");
+        println!("cargo:rustc-link-lib=dylib=fftw3f");
 
-    let ayf_sdk_path = std::env::var("AYF_SDK_PATH").expect("AYF_SDK_PATH is not set");
-    println!("cargo:rustc-link-search=native={}/lib/", ayf_sdk_path);
+        let ayf_sdk_path = std::env::var("AYF_SDK_PATH").expect("AYF_SDK_PATH is not set");
+        println!("cargo:rustc-link-search=native={}/lib/", ayf_sdk_path);
+    }
 }
