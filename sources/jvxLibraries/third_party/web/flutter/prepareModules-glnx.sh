6c7764df1116e1847048c70d5f42f5b0351753d6
Florian Wickert <FlorianWickert@gmail.com>
2025-01-22 12:11:14 +0100

    flutter: update to 3.7.5 and clean-up scripts

--- sources/jvxLibraries/third_party/web/flutter/prepareModules-glnx.sh
+++ sources/jvxLibraries/third_party/web/flutter/prepareModules-glnx.sh
@@ -1,13 +1,9 @@
-#!/bin/bash  
-
-if [ ! -d "flutter-3.7.4" ]; then
-
-	curl -kLSs  https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.7.4-stable.tar.xz -o flutter.tar.xz	
+#!/bin/bash
+if [ ! -d "flutter-${JVX_FLUTTER_VERSION}" ]; then
+	curl -kLSs  https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${JVX_FLUTTER_VERSION}-stable.tar.xz -o flutter.tar.xz
 	xz -d flutter.tar.xz
-	tar -xf flutter.tar 
+	tar -xf flutter.tar
 	sleep 1
-	mv flutter flutter-3.7.4
+	mv flutter flutter-${JVX_FLUTTER_VERSION}
 	rm flutter.tar
 fi
-
-
--- sources/jvxLibraries/third_party/web/flutter/prepareModules-win.sh
+++ sources/jvxLibraries/third_party/web/flutter/prepareModules-win.sh
@@ -1,27 +1,23 @@
-#!/bin/bash  
-
-if [ ! -d "flutter-3.7.4" ]; then
-
+#!/bin/bash
+if [ ! -d "flutter-${JVX_FLUTTER_VERSION}" ]; then
 	if [ ! -d "flutter" ]; then
-		curl -kLSs  https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.7.4-stable.zip -o flutter.zip	
+		curl -kLSs  https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_${JVX_FLUTTER_VERSION}-stable.zip -o flutter.zip
 		unzip flutter.zip
-	fi 
-	
+	fi
+
 	sleep 5
-	
+
 	max=10
 	for i in {1..$max}
 	do
-		if [ ! -d "flutter-3.7.4" ]; then
-			echo "Trying to mv flutter to flutter-3.7.4, rum <$i>"
-			mv flutter flutter-3.7.4
-			if [ ! -d "flutter-3.7.4" ]; then
+		if [ ! -d "flutter-${JVX_FLUTTER_VERSION}" ]; then
+			echo "Trying to mv flutter to flutter-${JVX_FLUTTER_VERSION}, rum <$i>"
+			mv flutter flutter-${JVX_FLUTTER_VERSION}
+			if [ ! -d "flutter-${JVX_FLUTTER_VERSION}" ]; then
 				sleep 5
-			fi 
-		fi 
+			fi
+		fi
 	done
-		
+
 	rm flutter.zip
 fi
-
-
