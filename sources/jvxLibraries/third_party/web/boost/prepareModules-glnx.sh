02f8932bafc2aa099e64917d0e2387bb56dfa5f3
Hauke Krüger <hk@binauricsaudio.com>
2025-02-17 18:27:25 +0100

    Changed location of boost download

--- sources/jvxLibraries/third_party/web/boost/prepareModules-glnx.sh
+++ sources/jvxLibraries/third_party/web/boost/prepareModules-glnx.sh
@@ -4,17 +4,19 @@ if [ ! -d "boost" ]; then
 
 	if [ ! -f "boost-1.70.0.7z" ]; then
 		echo "1) Downloading boost binary package - this may really take some time!!"
-		curl -kLSs https://boostorg.jfrog.io/artifactory/main/release/1.70.0/binaries/boost_1_70_0-unsupported-bin-msvc-all-32-64.7z -o boost-1.70.0.7z
+		wget https://archives.boost.io/release/1.70.0/source/boost_1_70_0.zip
 	fi 
 
-	# We need to "sudo apt install 7zip"
 	echo "2) Extracting boost binary package"
-	7zz x boost-1.70.0.7z
+	unzip -q boost_1_70_0.zip
+	
+	pwd 
+	ls
 	
 	echo "3) Renaming boost folder"
 	mv boost_1_70_0 boost
 
 	echo "4) Removing downloaded packages"
-	rm boost-1.70.0.7z
+	rm boost_1_70_0.zip
 	
 fi
