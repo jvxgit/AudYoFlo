9d5c1d3fa0d87eeb44940bc8417bc1f56f7d37ea
Hauke Krüger <hk@javox-solutions.com>
2025-05-03 21:46:04 +0200

    Introduced new name for patch dos2unix file

--- sources/jvxLibraries/third_party/git/mysofa/prepareModules-win.sh
+++ sources/jvxLibraries/third_party/git/mysofa/prepareModules-win.sh
@@ -6,8 +6,8 @@ if [ ! -d "libmysofa" ]; then
 	cd libmysofa
 		git checkout  bed445ba48ad9faf0e758b54af3f6075949d27fe
 		# cp ../build-scripts/compile.bat build
-		dos2unix ../0001-Added-cmake-option-to-activate-log-output.patch
-		git apply ../0001-Added-cmake-option-to-activate-log-output.patch
+		dos2unix -n ../0001-Added-cmake-option-to-activate-log-output.patch localpatch.patch
+		git apply localpatch.patch
 	cd ..
 	
 	# Really hard to patch this - patch tools are really annoying. Something with linefeeds
