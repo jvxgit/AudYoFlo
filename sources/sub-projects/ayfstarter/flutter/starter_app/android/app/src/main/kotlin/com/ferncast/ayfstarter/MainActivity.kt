4286f150bae188e725ac9598369bde49c4628e4a
Hauke Krüger <hk@binauricsaudio.com>
2025-05-20 08:35:28 +0200

    Added Android code to starter project

---
+++ sources/sub-projects/ayfstarter/flutter/starter_app/android/app/src/main/kotlin/com/ferncast/ayfstarter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ferncast.ayfstarter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ sources/sub-projects/ayfstarter/flutter/starter_app/example/android/app/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.ayfbinrender_example"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+    
+    // Override the default NDK version to prevent warning. flutter.ndkVersion is set somewhere in the Flutter package,
+    // https://stackoverflow.com/questions/77228813/where-is-defined-flutter-ndkversion-in-build-gradle
+    // Currently, it is set to flutter.ndkVersion=26.3.11579264
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.ayfbinrender_example"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
