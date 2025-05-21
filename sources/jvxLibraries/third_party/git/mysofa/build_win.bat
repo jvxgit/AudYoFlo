0bb00bca6f51edeedb9c89f1dc27e09080bb729b
Hauke Krüger <hk@binauricsaudio.com>
2025-05-21 13:07:25 +0200

    Extended configure function in sofa lib to support x64 and win32 on one PC

--- sources/jvxLibraries/third_party/git/mysofa/build_win.bat
+++ sources/jvxLibraries/third_party/git/mysofa/build_win.bat
@@ -4,7 +4,9 @@ set target_folder=%~1
 set buildtype=%~2
 set arch_simple=%~3
 
-cd %target_folder%\build
-cmake -DCMAKE_BUILD_TYPE=%buildtype% .. -G "Ninja" -DBUILD_TESTS=FALSE -DBUILD_SHARED_LIBS=FALSE -DCMAKE_INSTALL_PREFIX=%arch_simple%/%buildtype% -Wno-dev
+echo "Running build script in %target_folder%\build_%arch_simple%
+
+cd %target_folder%\build_%arch_simple%
+cmake -DCMAKE_BUILD_TYPE=%buildtype% .. -G "Ninja" -DBUILD_TESTS=FALSE -DBUILD_SHARED_LIBS=FALSE -DCMAKE_INSTALL_PREFIX=%buildtype% -Wno-dev
 ninja install
 
