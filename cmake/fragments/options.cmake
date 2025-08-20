9f7c8fb3c8e88860b6c3bfe2b913b8e3c0c89528
Hauke Krüger <hk@binauricsaudio.com>
2025-08-20 08:29:44 +0200

    Added option to not compile sc594 code within cmake process - it just takes too long.

--- cmake/fragments/options.cmake
+++ cmake/fragments/options.cmake
@@ -22,6 +22,7 @@ option(JVX_RELEASE_C674X "Compile c674x DSP applications (requires CCS and DSP/B
 option(JVX_RELEASE_ARMCORTEXA8 "Compile ARM Cortex A8 applications (requires CCS and ARM-GCC)" false)
 option(JVX_RELEASE_ARMCORTEXM4 "Compile ARM Cortex M4 applications (requires NXP MCUXpresso)" false)
 option(JVX_RELEASE_ADIDSP_SC594 "Compile ADI DSP SC594 applications (requires Cces)" false)
+option(AYF_SC594_SKIP_CCES_BUILD "Skip building cces build for sc594 in the course of the cmake build proces" true)
 option(JVX_USE_OPENGL "Compile applications and components using opengl in combination with x11 (freeglut and glew)" false)
 # option(JVX_USE_OPENGL_X11 "Compile applications and components using opengl in combination with x11 (freeglut and glew)" false)
 option(JVX_USE_OPENCV "Compile applications and components using opencv" false)
