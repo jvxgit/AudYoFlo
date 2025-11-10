70f1af52fedfccc0339f8b380dffd1269d5f1e8d
Hauke Krüger <hk@binauricsaudio.com>
2025-11-10 12:20:10 +0100

    Minor bugfixes in build system found in clean rebuild of release version

--- sources/jvxLibraries/third_party/git/portaudio/prepareModules-win.sh
+++ sources/jvxLibraries/third_party/git/portaudio/prepareModules-win.sh
@@ -14,7 +14,7 @@ if [ ! -d "portaudio" ]; then
 	
 	curl -kLSs https://www.steinberg.net/asiosdk?_ga=2.34212833.1787253143.1617615529-938406583.1617615529asiosdk_2.3.3_2019-06-14.zip -o asiosdk_2.3.3_2019-06-14.zip
 	unzip asiosdk_2.3.3_2019-06-14.zip		
-	mv asiosdk_2.3.3_2019-06-14 portaudio/src/hostapi/asio/ASIOSDK
+	mv ASIOSDK portaudio/src/hostapi/asio/
 	rm asiosdk_2.3.3_2019-06-14.zip
 
 fi
