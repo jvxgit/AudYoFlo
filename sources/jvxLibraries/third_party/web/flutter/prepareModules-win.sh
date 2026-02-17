8a15f16e29eb12bcac7051ba1b0c283301febdca
Hauke Krüger <hk@binauricsaudio.com>
2026-02-17 07:54:27 +0100

    Hopefully fixed the issue with windows not being able to access extracted archives "sometimes".

--- sources/jvxLibraries/third_party/web/flutter/prepareModules-win.sh
+++ sources/jvxLibraries/third_party/web/flutter/prepareModules-win.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
+JVX_FLUTTER_VERSION=$1
+
+echo Running download to receive flutter version ${JVX_FLUTTER_VERSION}
+ 
 if [ ! -d "flutter-${JVX_FLUTTER_VERSION}" ]; then
 	if [ ! -d "flutter" ]; then
-		curl -kLSs  https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_${JVX_FLUTTER_VERSION}-stable.zip -o flutter.zip
-		unzip flutter.zip
+		wget https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_${JVX_FLUTTER_VERSION}-stable.zip 
+		unzip flutter_windows_${JVX_FLUTTER_VERSION}-stable.zip
 	fi
 
 	sleep 5
 
 	max=10
-	for i in {1..$max}
-	do
-		if [ ! -d "flutter-${JVX_FLUTTER_VERSION}" ]; then
-			echo "Trying to mv flutter to flutter-${JVX_FLUTTER_VERSION}, rum <$i>"
-			mv flutter flutter-${JVX_FLUTTER_VERSION}
-			if [ ! -d "flutter-${JVX_FLUTTER_VERSION}" ]; then
-				sleep 5
-			fi
+	target="flutter-${JVX_FLUTTER_VERSION}"
+
+	for ((i=1; i<=max; i++)); do
+		if [ -d "$target" ]; then
+			echo "Target '$target' already exists; done."
+			break
+		fi
+
+		echo "Trying to mv flutter -> $target, run <$i>"
+
+		if mv -T flutter "$target" 2>/dev/null; then
+			echo "Rename succeeded."
+			break
+		else
+			# optional: Fehlermeldung anzeigen
+			echo "mv failed (run <$i>), retrying..." >&2
+			sleep 5
 		fi
 	done
-
+	
 	rm flutter.zip
 fi
