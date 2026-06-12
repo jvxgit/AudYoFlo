d21d3f60772146388163206d624d29eb061bb38c
Hauke Krueger <hk@binauricsaudio.com>
2026-06-12 14:34:13 +0200

    Some corrections for Linux build - Ubuntu 26.04 LTS

--- cmake/fragments/thrdparty-early-external.cmake
+++ cmake/fragments/thrdparty-early-external.cmake
@@ -2,6 +2,8 @@ message("Running code to install third party libs at very early time.")
 
 # Only if we compile the build tools
 if(JVX_BUILD_BISON)
-	set(JVX_BASE_3RDPARTY_LIBS_EARLY_EXTERNAL ${JVX_BASE_3RDPARTY_LIBS_EARLY_EXTERNAL}
-		${JVX_SUBPRODUCT_ROOT}/sources/jvxLibraries/third_party/git/flexbison)	
+	if(${JVX_OS} MATCHES "windows")
+		set(JVX_BASE_3RDPARTY_LIBS_EARLY_EXTERNAL ${JVX_BASE_3RDPARTY_LIBS_EARLY_EXTERNAL}
+			${JVX_SUBPRODUCT_ROOT}/sources/jvxLibraries/third_party/git/flexbison)
+	endif()
 endif()
--- sources/jvxLibraries/third_party/git/opencv/prepareModules.sh
+++ sources/jvxLibraries/third_party/git/opencv/prepareModules.sh
@@ -4,6 +4,7 @@ if [ ! -d "opencv" ]; then
 	git clone https://github.com/opencv/opencv.git
 	cd opencv
 		# git checkout 1ca526dcdb9c30600c70537e279f0c672057a1b9
+		git checkout cadcb7e4e0a04867a0996a35c62960de8aa6da47 
 	cd ..
 fi
 
